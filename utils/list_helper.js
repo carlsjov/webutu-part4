3832c942fd67feeed754637c2591086745eaaf15
Carl Sjövall <caersj@utu.fi>
2024-04-15 23:30:15 +0300

    part4-4 tehty

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) => {
+    const likeArray = blogs.map(blog => blog.likes)
+    const total = likeArray.reduce((sum, like) => sum+like, 0)
+    return total;
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
