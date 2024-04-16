3f17f7f935bb0b0946bfb3d96ce3efe57aa39730
Carl Sjövall <caersj@utu.fi>
2024-04-16 22:36:13 +0300

    muutettu blog

--- models/blog.js
+++ models/blog.js
@@ -1,14 +1,4 @@
-const config = require('../utils/config')
-const logger = require('../utils/logger')
 const mongoose = require('mongoose')
-mongoose.set('strictQuery', false)
-
-const url = config.MONGODB_URI
-mongoose.connect(url).then(res => {
-    logger.info('connected to MongoDB')
-}).catch((error) => {
-    logger.info('error connecting to MongoDB:', error.message)
-})
 
 const blogSchema = mongoose.Schema({
     title: String,
