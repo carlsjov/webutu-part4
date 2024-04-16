8bdf7e3c80746bd01012f9002592f0827a5675a1
Carl Sjövall <caersj@utu.fi>
2024-04-16 22:34:15 +0300

    muutettu index

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
 const config = require('./utils/config')
-//const Blog = require('./models/blog')
-const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
-})
+app.listen(config.PORT), () => {
+  logger.info(`Server running on port ${config.PORT}`)
+}
