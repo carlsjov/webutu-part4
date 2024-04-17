ef1be4ae2b7322656c9931563a69e98c8e82441f
Carl Sjövall <caersj@utu.fi>
2024-04-17 19:48:24 +0300

    part4-11 tehty

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
@@ -23,5 +24,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
