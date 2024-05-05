080097708a505f2cca67e70f061707641d4e9a51
Carl Sjövall <caersj@utu.fi>
2024-05-05 14:36:21 +0300

    part5-1 tehty

--- app.js
+++ app.js
@@ -25,5 +25,6 @@ app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use(express.static('dist'))
 
 module.exports = app
